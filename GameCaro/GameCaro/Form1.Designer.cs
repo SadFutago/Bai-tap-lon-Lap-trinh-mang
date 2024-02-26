fdb37fb43bdccb1bb29d2f49db01b1fb0dc9c6c4
SadFutago <son92374@st.vimaru.edu.vn>
2024-02-26 20:59:03 +0700

    Tao ban co caro

---
+++ GameCaro/GameCaro/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace GameCaro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.panelAvatar = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect_LAN = new System.Windows.Forms.Button();
+            this.txbIP_Address = new System.Windows.Forms.TextBox();
+            this.pctbMark = new System.Windows.Forms.PictureBox();
+            this.procCoolDown = new System.Windows.Forms.ProgressBar();
+            this.txbPlayerName = new System.Windows.Forms.TextBox();
+            this.panelAvatar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.BackColor = System.Drawing.SystemColors.Info;
+            this.panelBoard.Location = new System.Drawing.Point(12, 12);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(766, 631);
+            this.panelBoard.TabIndex = 0;
+            // 
+            // panelAvatar
+            // 
+            this.panelAvatar.Controls.Add(this.pictureBox1);
+            this.panelAvatar.Location = new System.Drawing.Point(816, 12);
+            this.panelAvatar.Name = "panelAvatar";
+            this.panelAvatar.Size = new System.Drawing.Size(488, 328);
+            this.panelAvatar.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 253);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.PeachPuff;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.btnConnect_LAN);
+            this.panel3.Controls.Add(this.txbIP_Address);
+            this.panel3.Controls.Add(this.pctbMark);
+            this.panel3.Controls.Add(this.procCoolDown);
+            this.panel3.Controls.Add(this.txbPlayerName);
+            this.panel3.Location = new System.Drawing.Point(816, 346);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(488, 297);
+            this.panel3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(15, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Địa chỉ IP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(15, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Tên người chơi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thời gian còn lại";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Đến lượt của";
+            // 
+            // btnConnect_LAN
+            // 
+            this.btnConnect_LAN.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect_LAN.Location = new System.Drawing.Point(109, 212);
+            this.btnConnect_LAN.Name = "btnConnect_LAN";
+            this.btnConnect_LAN.Size = new System.Drawing.Size(94, 46);
+            this.btnConnect_LAN.TabIndex = 4;
+            this.btnConnect_LAN.Text = "Kết nối";
+            this.btnConnect_LAN.UseVisualStyleBackColor = true;
+            // 
+            // txbIP_Address
+            // 
+            this.txbIP_Address.Location = new System.Drawing.Point(18, 173);
+            this.txbIP_Address.Name = "txbIP_Address";
+            this.txbIP_Address.Size = new System.Drawing.Size(185, 22);
+            this.txbIP_Address.TabIndex = 3;
+            // 
+            // pctbMark
+            // 
+            this.pctbMark.Location = new System.Drawing.Point(255, 82);
+            this.pctbMark.Name = "pctbMark";
+            this.pctbMark.Size = new System.Drawing.Size(208, 186);
+            this.pctbMark.TabIndex = 2;
+            this.pctbMark.TabStop = false;
+            // 
+            // procCoolDown
+            // 
+            this.procCoolDown.Location = new System.Drawing.Point(18, 116);
+            this.procCoolDown.Name = "procCoolDown";
+            this.procCoolDown.Size = new System.Drawing.Size(185, 23);
+            this.procCoolDown.TabIndex = 1;
+            // 
+            // txbPlayerName
+            // 
+            this.txbPlayerName.Location = new System.Drawing.Point(18, 57);
+            this.txbPlayerName.Name = "txbPlayerName";
+            this.txbPlayerName.Size = new System.Drawing.Size(185, 22);
+            this.txbPlayerName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 694);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panelAvatar);
+            this.Controls.Add(this.panelBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Caro";
+            this.panelAvatar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctbMark)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBoard;
+        private System.Windows.Forms.Panel panelAvatar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnConnect_LAN;
+        private System.Windows.Forms.TextBox txbIP_Address;
+        private System.Windows.Forms.PictureBox pctbMark;
+        private System.Windows.Forms.ProgressBar procCoolDown;
+        private System.Windows.Forms.TextBox txbPlayerName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
